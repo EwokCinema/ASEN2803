cf9faf780d1c23977c0fe58da68621004d0b766d
EwokCinema <omar.gaytan@colorado.edu>
2024-06-13 15:44:22 -0600

    Update straightTrack.m

--- straightTrack.m
+++ straightTrack.m
@@ -19,12 +19,12 @@ function [position, velocity, accel] = straightTrack(theta,v_0,g,s_0,s_f,num_poi
 %--------------------------------------------------------------------------
 
 line = sqrt((s_f(1)-s_0(1))^2 + (s_f(2)-s_0(2))^2 +(s_f(3)-s_0(3))^2); % distance (magnitude)
-track = linspace(0,line,num_points);
+track = linspace(0,line,num_points); % creating linspace for track
 
 position = zeros(3,num_points); % creating zero matrix for track lengths aka position
 velocity = zeros(3,num_points); % creating zero vector for velocity at each point
 accel = zeros(3,num_points); % creating zero matrix for acceleration at each point
-v_mag = zeros(num_points);
+v_mag = zeros(num_points); % creating zero vector for velocity magnitude at any point
 
 x_test = s_f(1) - s_0(1); % deciding which axis is not being used
 
